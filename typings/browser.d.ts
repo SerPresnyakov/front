38d98446791a271593671e4b6a862c8037c58445
Александр Кондауров <kondaurov@realweb.ru>
2016-05-19 17:11:58 +0300

    refactor

--- typings/browser.d.ts
+++
@@ -1,7 +0,0 @@
-/// <reference path="browser/ambient/angular-formly/index.d.ts" />
-/// <reference path="browser/ambient/angular-local-storage/index.d.ts" />
-/// <reference path="browser/ambient/angular-material/index.d.ts" />
-/// <reference path="browser/ambient/angular-ui-router/index.d.ts" />
-/// <reference path="browser/ambient/angular/index.d.ts" />
-/// <reference path="browser/ambient/jquery/index.d.ts" />
-/// <reference path="browser/ambient/tsmonad/index.d.ts" />
